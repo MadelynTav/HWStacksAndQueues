102cdac5ab6f7dde3cbc28983241745155b8b0e3
Estefania Ortiz <eaortiz@stanford.edu>
2015-11-10 12:15:17 -0500

    Update EvenWordHW.java

--- EvenWordHW.java
+++ EvenWordHW.java
@@ -8,7 +8,7 @@ public class EvenWordHW
 {
     public static boolean isEvenWord(String word)
     {
-        if(word.length()==0 || word.isEmpty()){
+        if(word.length()==0 || word.isEmpty()){ //same thing, why are you asking it twice?
             return true;
         }
          else {
@@ -17,12 +17,13 @@ public class EvenWordHW
             if(subWord.contains(current))
             {
                 subWord = subWord.replaceFirst(current, "");
-                    return isEvenWord(subWord);
+                return isEvenWord(subWord);
             }
+            return false; //it doesn't make a difference in this case, but semantically it makes more sense for the return false to be here
         }
-        return false;
     }
 
+    // I would love to see test cases here
     public static void main(String[] args)
     {
         System.out.println("Enter A Word Whose Evenness You'd Like To Know");
