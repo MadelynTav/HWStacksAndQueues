25e8be854f179488a92a10f16ab6475eebac0e82
MadelynTav <Madelyntav@gmail.com>
2015-12-01 20:06:07 -0500

    adding missing number

---
+++ MissingNumber.java
@@ -0,0 +1,34 @@
+package nyc.c4q.madelyntav;
+/**
+ * Created by c4q-madelyntavarez on 12/1/15.
+ */
+public class MissingNumber
+{
+
+    public static int missingNumberInArray(int [] array){
+        int [] newArray=new int[array.length+2];
+
+        for(int i: array){
+            newArray[i]=i;
+        }
+
+       return findMissingNum(newArray);
+    }
+
+    public static int findMissingNum(int [] numbers){
+        int missingNum =-1;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]==0){
+                missingNum=i;
+            }
+        }
+        return missingNum;
+    }
+
+    public static void main(String[] args)
+    {
+        int [] myArray={10,6,7,5,3,2,4,8,9};
+        System.out.println(missingNumberInArray(myArray));
+
+    }
+}
